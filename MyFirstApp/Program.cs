d081449b39e0ac08570875f1a1bcdf537a1e145e
Toomas J. <tom.juri@hotmail.com.>
2023-09-16 12:35:48 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// rakendus küsib kasutajal sisestada tema nimi
+// rAKENDUS TERVITAB KASUTAJAT NIMEPIDI
+
+Console.WriteLine("Enter your name:"); // Output
+// string - sõne
+string userName;
+
+userName = Console.ReadLine();  // input
+
+Console.WriteLine("Hello" + ", " + userName +"!"); // Output
+// string interpolation
+
+
+Console.WriteLine($"Hello, {userName}!");  // Output  
